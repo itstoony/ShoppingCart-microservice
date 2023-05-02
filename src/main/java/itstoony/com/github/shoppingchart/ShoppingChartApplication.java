be688b07bc33c0a08b2bd280a029cab1090e72ec
tony <toonyrenner@gmail.com>
2023-05-02 15:08:29 -0300

    Creating ShoppingChart microservice.

---
+++ src/main/java/itstoony/com/github/shoppingchart/ShoppingChartApplication.java
@@ -0,0 +1,13 @@
+package itstoony.com.github.shoppingchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingChartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingChartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/itstoony/com/github/shoppingchart/ShoppingChartApplicationTests.java
@@ -0,0 +1,13 @@
+package itstoony.com.github.shoppingchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingChartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
